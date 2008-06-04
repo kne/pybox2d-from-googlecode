c81a97f8f7754c91564910b84b4ed86543082884
sirkne <sirkne@bc78602e-eb4b-0410-95f1-012f1fce2112>
2008-06-04 07:09:12 +0000

    A few clean-ups for the TriangleMesh/BreakableBody test.

--- trunk/testbed/TriangleMesh.py
+++ trunk/testbed/TriangleMesh.py
@@ -5,19 +5,10 @@
 # 
 # Implemented using the pybox2d SWIG interface for Box2D (pybox2d.googlepages.com)
 # 
-# This software is provided 'as-is', without any express or implied
-# warranty.  In no event will the authors be held liable for any damages
-# arising from the use of this software.
-# Permission is granted to anyone to use this software for any purpose,
-# including commercial applications, and to alter it and redistribute it
-# freely, subject to the following restrictions:
-# 1. The origin of this software must not be misrepresented; you must not
-# claim that you wrote the original software. If you use this software
-# in a product, an acknowledgment in the product documentation would be
-# appreciated but is not required.
-# 2. Altered source versions must be plainly marked as such, and must not be
-# misrepresented as being the original software.
-# 3. This notice may not be removed or altered from any source distribution.
+# Note that this code does not follow the zlib license like the rest of the code does.
+#
+# See the license below if you are planning on distributing a project that uses this
+# code.
 #
 # -----------------------------------------------------------------------------
 # Original comments (ported from the C++ version):
@@ -56,7 +47,7 @@
 
 from math import sin, cos, atan2, sqrt, pow, floor, log
 
-# errors and warnings - TODO
+# errors and warnings
 tmE_OK    =0
 tmE_MEM   =1
 tmE_HOLES =2
@@ -129,7 +120,7 @@ class TriangleMesh(object):
     def SetOptions(self, options): self.options = options
     def AddOption(self, options):  self.options |= options
 
-    # angle in degree !
+    # angle in degrees!
     def SetAngle(self, angle): self.minAngle = angle
     def GetVertexCount(self):          return self.vertexCount        
     def GetInputVertexCount(self):     return self.inputVertexCount   
