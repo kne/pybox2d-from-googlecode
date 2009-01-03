170a1e04ecb4263c4d8b96eff88eb5f50669e1b4
sirkne <sirkne@bc78602e-eb4b-0410-95f1-012f1fce2112>
2009-01-03 00:12:32 +0000

    Still not quite working.

--- branches/new_setup/setup.py
+++ branches/new_setup/setup.py
@@ -18,9 +18,9 @@ from glob import glob
 
 # release version number
 box2d_version = "2.0.2"
-release_number = 0
+release_number = 1
 
-# Create the version string
+# create the version string
 version_str = "%sb%s" % (box2d_version, str(release_number))
 
 source_paths = [
@@ -30,46 +30,54 @@ source_paths = [
     'Box2D/Dynamics/Joints/',
     'Box2D/Common/',
     'Box2D/Collision/',
+    'Box2D/Collision/Shapes/',
     ]
 
 # glob all of the paths and then flatten the list into one
-box2d_source_files = sum( [glob(os.path.join(path, "*.cpp")) for path in source_paths], [])
+box2d_source_files = ['Box2D/Box2D.i'] + \
+    sum( [glob(os.path.join(path, "*.cpp")) for path in source_paths], [])
 
+# arguments to pass to SWIG. for old versions of SWIG, -O (optimize) might not be present.
 swig_arguments = '-c++ -O -includeall -ignoremissing -w201'
 
-setup (name = "Box2D",
-    version = version_str,
-    author      = "kne",
-    author_email = "sirkne at gmail dot com",
-    description = "Python Box2D",
-    license="zlib",
-    url="http://pybox2d.googlecode.com/",
-    long_description = """Box2D (version %s) for usage in Python.
+pybox2d_extension = \
+    Extension('_Box2D', box2d_source_files, extra_compile_args=['-I.'], language='c++')
 
-After installing, please be sure to try out the testbed demos (requires pygame).
-See <python directory>box2d/testbed/demos.py .
+LONG_DESCRIPTION = \
+"""Box2D (version %s) for usage in Python.
 
-For more information, see:
+    After installing please be sure to try out the testbed demos.
+    They require either pygame or pyglet and are available on the
+    homepage.
 
-pybox2d homepage: http://pybox2d.googlecode.com
-Box2D's homepage: http://www.box2d.org
-    """ % (box2d_version),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: zlib/libpng License",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: POSIX",
-        "Programming Language :: Python",
-        "Games :: Physics Libraries"
-    ],
-       
-#    packages = ['Box2D'],
-#    package_dir = {'Box2D': 'Box2D'},
-    py_modules = ['Box2D'],
-    options={'build_ext':{'swig_opts':swig_arguments}},
-    ext_modules = [Extension('_Box2D', ['Box2D/Box2D.i'] + box2d_source_files,
-                    extra_compile_args=["-I."], language="c++"),]
+    pybox2d homepage: http://pybox2d.googlecode.com
+    Box2D's homepage: http://www.box2d.org
+    """ % (box2d_version)
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: zlib/libpng License",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: POSIX",
+    "Programming Language :: Python",
+    "Games :: Physics Libraries"
+    ]
+
+setup_dict = dict(
+    name             = "Box2D",
+    version          = version_str,
+    author           = "kne",
+    author_email     = "sirkne at gmail dot com",
+    description      = "Python Box2D",
+    license          = "zlib",
+    url              ="http://pybox2d.googlecode.com/",
+    long_description = LONG_DESCRIPTION,
+    classifiers      = CLASSIFIERS,
+    options          = { 'build_ext': { 'swig_opts' : swig_arguments } },
+    ext_modules      = [ pybox2d_extension ]
     )
 
+# run the actual setup from distutils
+setup( **setup_dict )
